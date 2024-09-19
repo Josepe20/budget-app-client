6ae63c6540f5f9888099ce0c1f38130eab21e181
Jose Chay <chepechay16@gmail.com>
2024-09-19 11:45:39 -0600

    [UPDATE][update axiosConfig.ts envURL]

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -4,10 +4,9 @@ import { attachExceptionInterceptor } from './middlewares/exception.middleware';
 
 const envURL = enviroment === 'DEV' ? 'http://127.0.0.1:8000/api/' : 'I have no Prod enviroment yet XD'
 
-const baseURL = envURL;
 
 const instance = axios.create({
-  baseURL: baseURL, 
+  baseURL: envURL, 
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
